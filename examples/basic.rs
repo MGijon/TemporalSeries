d5c69d102d2ccf39e559eca44d0a33dfd5e01931
Manuel Gijón Agudo <mgijon94@gmail.com>
2026-05-29 15:50:32 +0200

    add explicit types to example

--- examples/basic.rs
+++ examples/basic.rs
@@ -1,13 +1,13 @@
 use temporalseries::series::TimeSeries;
 
 fn main() {
-    let index = vec![1, 2, 3, 4, 5];
-    let values = vec![100.0, 101.0, 102.0, 103.0, 104.0];
+    let index: Vec<i64> = vec![1, 2, 3, 4, 5];
+    let values: Vec<f64> = vec![100.0, 101.0, 102.0, 103.0, 104.0];
 
-    let time_serie = TimeSeries::new(index, values).unwrap();
+    let time_serie: TimeSeries = TimeSeries::new(index, values).unwrap();
 
-    let returns = time_serie.pct_change().unwrap();
-    let momentum = returns.rolling(2).mean().unwrap();
+    let returns: TimeSeries = time_serie.pct_change().unwrap();
+    let momentum: TimeSeries = returns.rolling(2).mean().unwrap();
 
     println!("{:?}", momentum.values);
 }
